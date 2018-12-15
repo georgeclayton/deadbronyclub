907a8a013aad748c880bc54432de85b8b5a53327
georgeclayton <georgewclayton@gmail.com>
2018-12-15 16:29:50 +0000

    adding in the data retrieval service module

---
+++ data-retrieval-service/src/main/java/com/dead/brony/club/dataretrievalservice/DataRetrievalServiceApplication.java
@@ -0,0 +1,14 @@
+package com.dead.brony.club.dataretrievalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataRetrievalServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataRetrievalServiceApplication.class, args);
+    }
+
+}
+
