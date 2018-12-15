c1952a44455099680469f27fa7d207325ba49add
georgeclayton <georgewclayton@gmail.com>
2018-12-15 15:05:52 +0000

    Added angular-cli.json to make intellij happy

--- bryptoClub/src/app/app.module.ts
+++ bryptoClub/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './sections/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
